500702421b2cd193c4d3509a9f772b23bb87ea76
Harry Charles Alfred Wright <harry.wright@and.digital>
2021-10-18 18:56:35 +0100

    Remove comments, less lines!

--- src/Challenge 12 - Number Rendering/index.ts
+++ src/Challenge 12 - Number Rendering/index.ts
@@ -6,11 +6,8 @@ const asciiNums = [
 	[" _ ", "|_|", "|_|"], [" _ ", "|_|", " _|"],
 ]
 const run = (input: number = 123456789) => {
-	// Split input into an array of numbers
 	const numbers = input.toString().split("").map(char => parseInt(char))
-	// Iteratively create a 2-dimensional array containing the numbers
 	const twoDArray = numbers.map(num => asciiNums[num])
-	// Transform the 2D array into a string
 	const outputString = twoDArray.reduce((prev, curr) => [ prev[0] + curr[0], prev[1] + curr[1], prev[2] + curr[2] ], ["", "", ""])
 	return outputString.reduce((prev, curr, i) => {
 		return `${prev}${i === 0 ? "" : "\n"}${curr}`
