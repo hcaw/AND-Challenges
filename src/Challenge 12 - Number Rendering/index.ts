2f6afb74ecad229fc7aef278b31014fdce70071c
Harry Charles Alfred Wright <harry.wright@and.digital>
2021-10-18 17:48:27 +0100

    Challenge completed

--- src/Challenge 12 - Number Rendering/index.ts
+++ src/Challenge 12 - Number Rendering/index.ts
@@ -1,28 +1,33 @@
-// Input: 1234567890
-// Output:
-//   _  _     _  _  _  _  _  _ 
-// | _| _||_||_ |_   ||_||_|| |
-// ||_  _|  | _||_|  ||_| _||_|
-const asciiNums = {
-	"0": [" _ ", "| |", "|_|"],
-	"1": [" ", "|", "|"],
-	"2": [" _ ", " _|", "|_ "],
-	"3": [" _ ", " _|", " _|"],
-	"4": ["   ", "|_|", "  |"],
-	"5": [" _ ", "|_ ", " _|"],
-	"6": [" _ ", "|_ ", "|_|"],
-	"7": [" _ ", "  |", "  |"],
-	"8": [" _ ", "|_|", "|_|"],
-	"9": [" _ ", "|_|", " _|"],
-}
+const asciiNums = [
+	[" _ ", "| |", "|_|"],
+	[" ", "|", "|"],
+	[" _ ", " _|", "|_ "],
+	[" _ ", " _|", " _|"],
+	["   ", "|_|", "  |"],
+	[" _ ", "|_ ", " _|"],
+	[" _ ", "|_ ", "|_|"],
+	[" _ ", "  |", "  |"],
+	[" _ ", "|_|", "|_|"],
+	[" _ ", "|_|", " _|"],
+]
 
 const run = (input: number) => {
 	// Split input into an array of numbers
 	const numbers = input.toString().split("").map(char => parseInt(char))
 	// Iteratively create a 2-dimensional array containing the numbers
-	const twoDArray = numbers.map(num => asciiNums[num.toString()])
+	const twoDArray = numbers.map(num => asciiNums[num])
 	// Transform the 2D array into a string
-	return twoDArray
+	const outputString = twoDArray.reduce((prev, curr) =>
+		[
+			prev[0] + curr[0],
+			prev[1] + curr[1],
+			prev[2] + curr[2]
+		]
+		, ["", "", ""])
+	return outputString.reduce((prev, curr, i) => {
+		const newline = i === 0 ? "" : "\n"
+		return `${prev}${newline}${curr}`
+	}, "")
 }
 
 const _input = 123456789
